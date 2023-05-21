45f63211ab55317acc46e4f42c46b55e1b7a5ddf
Ana Rocha <anasoledad.rocha@gmail.com>
2023-05-21 19:17:58 -0300

    Se agregó el state al mensaje

--- src/components/Mensaje.jsx
+++ src/components/Mensaje.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
 
-const Mensaje = ({nuevoMensaje}) => {
+const Mensaje = ({ nuevoMensaje }) => {
+    const [msj, setMsj] = useState();
+
     return (
-        <section className='bg-primary bg-opacity-25 py-5'>
-            <h1 className='text-center text-danger'>Hello {nuevoMensaje}</h1>
+        <section className='bg-success bg-opacity-25 py-5'>
+            <h1 className='text-center text-danger'>Hello {nuevoMensaje}{msj}!</h1>
+            <div className="d-flex justify-content-center">
+                <Button variant='primary' className="mt-3" onClick={() => setMsj(' (from changes state)')}>
+                    Click me
+                </Button>
+            </div>
         </section>
     );
 };
